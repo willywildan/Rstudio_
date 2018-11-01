5e6cd358075e0601add16c57e0333a6c7efdd5b4
willywildan <40651953+willywildan@users.noreply.github.com>
2018-11-02 06:35:09 +0700

    Update TUGAS.R

--- R_studio Matematika/tugas tugas + kuis (limit,integral)/TUGAS.R
+++ R_studio Matematika/tugas tugas + kuis (limit,integral)/TUGAS.R
@@ -2,7 +2,7 @@ value <- sample(x=1:1000,100,replace = FALSE, prob = NULL)
 
 G <- matrix (value, 10, 10, TRUE)
 G
-
+#transpose
 H <- t(G)
 H
 
@@ -12,7 +12,7 @@ J
 det(G)
 det(H)
 det(J)
-
+#matriks k = colom G 1-5 digabung colom G 1-5
 K <- cbind(G[,1:5] , J[,1:5])
 K
 
